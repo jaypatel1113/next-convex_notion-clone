e63d4e17bea16095223ae959b0c5eef49b935e34
Jay Patel <jay64441860@gmail.com>
2024-01-06 12:48:16 +0530

    all env updated and setup

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://central-rat-17.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
